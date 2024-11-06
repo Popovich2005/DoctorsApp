95871be3a1bc4e5317fbc827c49d54cbf14f2f6f
Popovich2005 <popich2005@mail.ru>
2024-11-06 14:07:44 +0300

    Initial Commit

---
+++ DoctorsApp/DoctorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsAppApp.swift
+//  DoctorsApp
+//
+//  Created by Алексей Попов on 06.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
